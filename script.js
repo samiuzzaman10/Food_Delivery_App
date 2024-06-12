6410029cd5dd698c3ae04ea3b82794bf2fae276e
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-06-12 12:51:52 +0600

    product section

---
+++ script.js
@@ -0,0 +1,14 @@
+// Product Section Code Start
+const imageArray = [
+  { url: "img/ice-creem.jpg" },
+  { url: "./img/Fried-Chicken.jpg" },
+  { url: "img/Kacchi-Biryani.jpg" },
+];
+const productBtn = document.querySelector(".product-section-btn");
+const productContainer = document.querySelector(".product-items-section");
+const hiddenDiv = document.querySelector(".hidden-div");
+
+productBtn.addEventListener("click", function showdiv() {
+    
+});
+// Product Section Code End 
